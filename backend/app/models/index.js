89c53d4b5968aa32049bec0eb9817d805807b2f8
Mikael <mikaelsdr@icloud.com>
2022-01-07 16:32:25 +0100

    Test 3
    ReferenceError: isModerator is not defined

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -5,7 +5,7 @@ const db = {}
 
 db.mongoose = mongoose
 
-db.user = require("./user.model");
+db.user = require("./user.role");
 db.role = require("./role.model");
 
 db.ROLES = ["user", "admin", "moderator"];
