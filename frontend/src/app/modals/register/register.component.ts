d698aa6b3d884140efd9537c9f9411924ff172e4
yunyan2020 <li_yunyan@hotmail.com>
2022-01-10 15:10:31 +0100

    change register UI with material

--- frontend/src/app/modals/register/register.component.ts
+++ frontend/src/app/modals/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email!: string;
+  username!:string;
+  password!: string;
+  repeatPassword!: string;
+  @Output() onRegisterEvent: EventEmitter<Object> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+ 
+  onRegister() {
+    const registerInfo = {
+      email: this.email,
+      username: this.username,
+      password: this.password,
+    }
+    this.onRegisterEvent.emit(registerInfo);
+
+    this.email = "",
+    this.username = "",
+    this.password = ""     
+
   }
 
 }
