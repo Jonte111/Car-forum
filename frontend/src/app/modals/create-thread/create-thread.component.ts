b76f71bf2138ecabfbf6512838af9cd7676f4cdf
Jonte111 <jonathan.pe@telia.com>
2022-01-12 13:53:15 +0100

    Made the modal and added if check to prevent empty fields.

--- frontend/src/app/modals/create-thread/create-thread.component.ts
+++ frontend/src/app/modals/create-thread/create-thread.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-thread.component.css']
 })
 export class CreateThreadComponent implements OnInit {
+  title!: string;
+  firstPost!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCreateThread() {
+    //Make sure that enter does not make a submit
+    console.log("click")
+    if (!this.title || !this.firstPost) {
+      return;
+    }
+    console.log(this.title, this.firstPost, " title and first post")
   }
 
 }
