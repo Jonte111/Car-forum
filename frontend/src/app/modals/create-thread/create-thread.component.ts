a37f06b994bf3fcc20134ddc5a307704d8e05b5d
Jonte111 <jonathan.pe@telia.com>
2022-01-17 14:34:45 +0100

    Removed dead code.

--- frontend/src/app/modals/create-thread/create-thread.component.ts
+++ frontend/src/app/modals/create-thread/create-thread.component.ts
@@ -16,30 +16,14 @@ export class CreateThreadComponent implements OnInit {
   }
 
   onCreateThread() {
-    console.log("click")
     if (!this.title || !this.firstPost) {
       return;
     }
-    console.log(this.title, this.firstPost, " title and first post")
-    // const createdThread = {
-    //   title: this.title,
-    //   token: localStorage.getItem('token'),
-    //   // firstPost: this.firstPost,
-    //   threadStarter: localStorage.getItem('username'),
-    //   posts: [this.firstPost],
-    //   threadViews: 0,
-    //   createdTime: Date,
-    //   numberOfComments: 0,
-    //   blockedUsers: [],
-    //   adminLocked: false,
-    //   threadStarterLocked: false,
-    // }
     const createdThreadInformation = {
       threadStarter: localStorage.getItem('id'),
       title: this.title,
       // firstPost: this.firstPost
     }
-    console.log(createdThreadInformation, " createdThreadInformation");
     
     this._store.createThread(createdThreadInformation);
 
