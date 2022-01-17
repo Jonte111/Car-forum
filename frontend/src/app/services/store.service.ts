b11c86a9340ffeca93efa422f95767a799e82bc3
Jonte111 <jonathan.pe@telia.com>
2022-01-17 15:38:22 +0100

    Started making createPost.

--- frontend/src/app/services/store.service.ts
+++ frontend/src/app/services/store.service.ts
@@ -6,12 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class StoreService {
   private urlToCreateThread = "api/thread/create";
+  private urlToCreatePost = "api/post/create";
   constructor(private http: HttpClient) { }
 
   createThread(threadToBeCreated: any) {
     return this.http.post(this.urlToCreateThread, threadToBeCreated).subscribe(
       res => console.log(res, " res"),
     )
-      
+  }
+  createPost(postToBeCreated: any) {
+    return this.http.post(this.urlToCreatePost, postToBeCreated).subscribe(
+      res => console.log(res, " res"),
+    )
   }
 }
