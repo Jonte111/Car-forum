8becc7939187f860a8f56eafe26da60fe5548a70
Jonte111 <jonathan.pe@telia.com>
2022-01-17 10:20:37 +0100

    Continued work on thread.
    Co-Authored-By: yunyan2020 <59655244+yunyan2020@users.noreply.github.com>

--- frontend/src/app/pages/sportcars/sportcars.component.ts
+++ frontend/src/app/pages/sportcars/sportcars.component.ts
@@ -8,7 +8,41 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./sportcars.component.css']
 })
 export class SportcarsComponent implements OnInit {
-  tempThreads = [{}]
+  tempThreads = [{
+    "id":"asdasdasd",
+    "threadStarter":"RandomName",
+    "posts": ["hej","test","äpple"],
+    "threadViews": "1111111",
+    "title": "This is the thread title",
+    "createdTime":"0111111111",
+    "numberOfComments":"3",
+    "blockedUsers":[],
+    "adminLocked":false,
+    "threadStarterLocked": false
+  }, {
+      "id": "asdasdasdasd",
+      "threadStarter": "RandomName",
+      "posts": ["hej", "test", "äpple"],
+      "threadViews": "1111111",
+      "title": "This is the secondthread title",
+      "createdTime": "0111111111",
+      "numberOfComments": "3",
+      "blockedUsers": [],
+      "adminLocked": false,
+      "threadStarterLocked": false
+    }, {
+      "id": "asdasdasdasdasdasd",
+      "threadStarter": "RandomName",
+      "posts": ["hej", "test", "äpple"],
+      "threadViews": "1111111",
+      "title": "This is the third thread title",
+      "createdTime": "0111111111",
+      "numberOfComments": "3",
+      "blockedUsers": [],
+      "adminLocked": false,
+      "threadStarterLocked": false
+    }
+  ]
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
