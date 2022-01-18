8e9c1e0bebec4a706f33eff1e272ea0bff9925b3
Jonte111 <jonathan.pe@telia.com>
2022-01-18 16:17:31 +0100

    Get threads from database based on categoryId.

--- frontend/src/app/pages/forum/forum.component.ts
+++ frontend/src/app/pages/forum/forum.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { CreateThreadComponent } from 'src/app/modals/create-thread/create-thread.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { StoreService } from 'src/app/services/store.service';
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
@@ -8,11 +11,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ForumComponent implements OnInit {
   categoryId!: string;
-  constructor(private route: ActivatedRoute) { }
+  public threads: any = [];
+
+  constructor(private route: ActivatedRoute,
+    public dialog: MatDialog,
+    private router: Router, private _store: StoreService) { }
 
   ngOnInit(): void {
     let id = this.route.snapshot.paramMap.get('id')!;
     this.categoryId = id;
+    this.getThreads(this.categoryId);
+  }
+  openCreateThread() {
+    this.dialog.open(CreateThreadComponent);
+  }
+
+  onSelectThread(thread: any) {
+    console.log("click")
+    this.router.navigate(['/forum/sportcars', thread.id])
+  }
+
+  getThreads(categoryId:any) {
+    this._store.getThreads(categoryId).subscribe(
+      res => {
+        this.threads = res,
+        console.log(this.threads, " this.threads get threads")
+      }
+    );
   }
 
 }
